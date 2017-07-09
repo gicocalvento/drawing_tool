29b5ec9b2c7190829746e74f03eb4e6022a80a4a
Gico <gcalvento@maltem.com>
2017-07-10 01:59:33 +0800

    rerendering objects to the canvas fix

--- js/app.js
+++ js/app.js
@@ -1,24 +1,32 @@
+// set canvas and context
 var canvas = document.getElementById("canvas-container");
 var ctx    = canvas.getContext("2d");
 
+// set variables
 const defaultWidth = 200;
 const defaultHeight = 200;
-var objects = new Array();
+var rectangles = new Array();
+var lines = new Array();
 
+
+// set default canvas width and height
 $( document ).ready(function() {
   canvas.width = defaultWidth;
   canvas.height = defaultHeight;
 });
 
+// resets the canvas
 $( "#resetCanvas" ).click(function( event ) {
 
-  objects = [];
+  rectangles = [];
+  lines = [];
   canvas.width = defaultWidth;
   canvas.height = defaultHeight;
   event.preventDefault();
 
 });
 
+// listen on properties form submit
 $( "#canvasPropertiesForm" ).submit(function( event ) {
 
   let width = $( "#canvasWidth" ).val();
@@ -28,6 +36,7 @@ $( "#canvasPropertiesForm" ).submit(function( event ) {
 
 });
 
+//create shapes on canvas
 $( "#canvasAttributesForm" ).submit(function( event ) {
 
   let x1 = $( "#x1" ).val();
@@ -39,19 +48,21 @@ $( "#canvasAttributesForm" ).submit(function( event ) {
   switch(action) {
     case "L":
       drawLine(x1,y1,x2,y2);
+      lines.push([x1,y1,x2,y2]);
       break;
     case "R":
       drawRectangle(x1,y1,x2,y2);
+      rectangles.push([x1,y1,x2,y2]);
       break;
     default:
     console.log("Please enter an action!");
   }
 
-  objects.push([x1,y1,x2,y2]);
   event.preventDefault();
 
 });
 
+// fill the canvas with color
 $( "#canvasFillForm" ).submit(function( event ) {
 
   let fillAction = $( "#fillAction" ).val();
@@ -63,7 +74,7 @@ $( "#canvasFillForm" ).submit(function( event ) {
   switch(fillAction) {
     case "B":
       bucketFill(color,fill1,fill2);
-      renderObjects(objects);
+      renderObjects(lines,rectangles);
       break;
     default:
     console.log("Please enter an action!");
@@ -73,19 +84,21 @@ $( "#canvasFillForm" ).submit(function( event ) {
 
 });
 
+// set canvas size
 const setCanvas = (width,height) => {
   canvas.width = width;
   canvas.height = height;
 };
 
+// draw lines
 const drawLine = (x1,y1,x2,y2) => {
-  var ctx = canvas.getContext("2d");
   ctx.beginPath();
   ctx.moveTo(x1, y1);
   ctx.lineTo(x2, y2);
   ctx.stroke();
 }
 
+// draw rectangles
 const drawRectangle = (x1,y1,x2,y2) => {
   ctx.rect(x1,y1,x2,y2);
   ctx.fillStyle = "white";
@@ -93,6 +106,7 @@ const drawRectangle = (x1,y1,x2,y2) => {
   ctx.stroke();
 }
 
+// bucket fill
 const bucketFill = (color,fill1,fill2) => {
 
   switch (color) {
@@ -114,11 +128,20 @@ const bucketFill = (color,fill1,fill2) => {
 
 }
 
-const renderObjects = (obj) => {
+// render objects
+const renderObjects = (lines,rectangles) => {
 
-  for (let i = 0; i < obj.length; i++) {
-    let params = obj[i].map(num => parseInt(num));
+  for (let i = 0; i < rectangles.length; i++) {
+    let params = rectangles[i].map(num => parseInt(num));
     ctx.clearRect(params[0],params[1],params[2],params[3]);
+  }
+
+  for (let i = 0; i < lines.length; i++) {
+    let params = lines[i].map(num => parseInt(num));
+    ctx.beginPath();
+    ctx.moveTo(params[0], params[1]);
+    ctx.lineTo(params[2], params[3]);
+    ctx.stroke();
   }
 
 }
